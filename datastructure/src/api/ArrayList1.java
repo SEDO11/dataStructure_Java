799b6f305a9a2f9a30920c4c076194382705beac
SEDO11 <do11anm@naver.com>
2022-03-15 12:21:59 +0900

    220315'

--- datastructure/src/api/ArrayList1.java
+++ datastructure/src/api/ArrayList1.java
@@ -13,7 +13,7 @@ public class ArrayList1 {
 		numbers.add(40);
 		System.out.println(numbers);
 		
-		numbers.add(1, 50);
+		numbers.add(1, 50); // 1번 인덱스에 값 50 저장
 		System.out.println(numbers);
 		
 		numbers.remove(2); //인덱스를 통해 해당 위치의 값 제거
@@ -29,10 +29,10 @@ public class ArrayList1 {
 		Iterator<Integer> it = numbers.iterator(); // Iterator = 인터페이스이다., 인터페이스의 객체를 it 변수에 저장
 		while(it.hasNext()) { //hasNext = 가져올 다음 값이 있는지 없는지 확인하는 메소드
 			int value = (int) it.next(); // 변수에 값 저장
-			if(value == 30) {
+			if(value == 30) { // 가져온 값이 30일 경우 해당 엘리멘트 제거
 				it.remove();
 			}
-			System.out.println(value);
+			System.out.println(value); 
 		}
 		System.out.println();
 		
